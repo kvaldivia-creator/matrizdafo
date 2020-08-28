67574cc6f9e0b5548cfb4efa435c2df981bfe347
Kevin Valdivia <kvaholman@gmail.com>
2020-08-28 06:38:29 -0500

    Updates

---
+++ precache-manifest.b88f3da9386621e29c2395c09a894294.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffc93826406e354a39c4b2121b92d60",
+    "url": "/matrizdafo/index.html"
+  },
+  {
+    "revision": "c1ba959a8956fef12b42",
+    "url": "/matrizdafo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "682e8dcf4b8a4ae3aa15",
+    "url": "/matrizdafo/static/css/main.42e02a88.chunk.css"
+  },
+  {
+    "revision": "c1ba959a8956fef12b42",
+    "url": "/matrizdafo/static/js/2.2dd57e1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/matrizdafo/static/js/2.2dd57e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682e8dcf4b8a4ae3aa15",
+    "url": "/matrizdafo/static/js/main.5ca0894b.chunk.js"
+  },
+  {
+    "revision": "8e99f0a7a79ad402e1e4",
+    "url": "/matrizdafo/static/js/runtime-main.148c8edc.js"
+  }
+]);
